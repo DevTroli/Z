74ec2ad10ba2eb2691092cb04c21d7e904225451
Pablo Troli <brawlixo123@gmail.com>
2025-03-07 01:48:35 -0300

    💄  (FrontEnd): Add UI/UX with tailwindCSS for User Registration

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from core.views import index, register, login
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/auth/", include("authentication.urls")),
+    path("", index, name="index"),
+    path("register/", register, name="register"),
+    path("login/", login, name="login"),
 ]
