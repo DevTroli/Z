52184a3aeef8b0966e29c5f09d0ed0606e132fa9
Troli <119256727+DevTroli@users.noreply.github.com>
2025-03-17 14:50:38 +0000

    🌐 (feat) configure new routes for zweet in global URLs 🚀

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,8 @@ from users.views import ZLoginView, ZSignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('zweets.urls')),
+    path('api/', include('zweets.api.urls')),
     path('login/', ZLoginView.as_view(), name='login'),
     path('signup/', ZSignupView.as_view(), name='signup'),
-    #path('api/', include('feed.api.urls')),  # Rotas DRF
 ]
