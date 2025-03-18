ee06f0e503f3f8060b6285a06f1a10fd4aa1d3f2
Pablo Troli <brawlixo123@gmail.com>
2025-03-17 22:18:35 -0300

    🚚 feat (routes) Add profile editing route

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import ZLoginView, ZSignupView
+from users.views import ZLoginView, ZSignupView, ZLogoutView, ProfileUpdateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('zweets.urls')),
-    path('api/', include('zweets.api.urls')),
-    path('login/', ZLoginView.as_view(), name='login'),
-    path('signup/', ZSignupView.as_view(), name='signup'),
+    path("admin/", admin.site.urls),
+    path("", include("zweets.urls")),
+    path("edit-profile/", ProfileUpdateView.as_view(), name="edit_profile"),
+    path("api/", include("zweets.api.urls")),
+    path("login/", ZLoginView.as_view(), name="login"),
+    path("signup/", ZSignupView.as_view(), name="signup"),
+    path("logout/", ZLogoutView.as_view(), name="logout"),
 ]
