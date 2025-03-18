678ed11a21e119ef879c8558dc1189c55d381450
Pablo Troli <brawlixo123@gmail.com>
2025-03-18 08:09:53 -0300

    ✅ Testes de integração para o perfil

--- zweets/tests.py
+++ zweets/tests.py
@@ -15,18 +15,3 @@ class ZweetAPITest(APITestCase):
         response = self.client.post(url, {"content": "Primeiro Zweet!"})
         self.assertEqual(response.status_code, 201)
         self.assertEqual(Zweet.objects.count(), 1)
-
-
-class FeedAuthTest(TestCase):
-    def test_zweet_form_visibility(self):
-        # Não autenticado - verifica texto parcial que existe no template
-        response = self.client.get(reverse("zweets:feed"))
-        self.assertContains(response, "Faça")
-        self.assertContains(response, "login")
-        self.assertContains(response, "para poder compartilhar seus Zweets")
-
-        # Autenticado'
-        user = User.objects.create_user(username="test")
-        self.client.force_login(user)
-        response = self.client.get(reverse("zweets:feed"))
-        self.assertContains(response, '<form id="zweetForm"')
