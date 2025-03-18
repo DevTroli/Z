aabfb02dfa9990335fb1c76af389346c6785a444
Pablo Troli <brawlixo123@gmail.com>
2025-03-18 08:48:13 -0300

    ✨ (feat) Add follow user functionality

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import ZLoginView, ZSignupView, ZLogoutView, ProfileUpdateView, ProfileView
+from users.views import ZLoginView, ZSignupView, ZLogoutView, ProfileUpdateView, ProfileView, FollowView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("zweets.urls")),
     path("@<str:username>/", ProfileView.as_view(), name="profile"),
     path("@<str:username>/edit", ProfileUpdateView.as_view(), name="edit_profile"),
+    path("follow/<str:username>/", FollowView.as_view(), name="follow"),
     path("api/", include("zweets.api.urls")),
     path("login/", ZLoginView.as_view(), name="login"),
     path("signup/", ZSignupView.as_view(), name="signup"),
