9e3e86e36dc61c8832c8e639bc126727d83daba8
Pablo Troli <brawlixo123@gmail.com>
2025-03-18 21:03:24 -0300

    🔥 Remove infinite scroll implementation

--- zweets/views.py
+++ zweets/views.py
@@ -1,16 +1,30 @@
 from django.views.generic import ListView, CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from .models import Zweet
 
 
-class FeedView(ListView):
+class FeedView(LoginRequiredMixin, ListView):
     model = Zweet
     template_name = "zweets/feed.html"
     context_object_name = "zweets"
     paginate_by = 10
 
     def get_queryset(self):
-        return Zweet.objects.select_related("user").prefetch_related("likes")[:20]
+        return Zweet.objects.select_related("user")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user"] = self.request.user
+        return context
+
+    def Get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page_obj = context["page_obj"]
+        context["paginator_range"] = page_obj.paginator.get_elided_page_range(
+            page_obj.number, on_each_side=1
+        )
+        return context
 
 
 class ZweetCreateView(CreateView):
