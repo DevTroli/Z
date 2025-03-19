945d243a8b9c1f61009625f2e78bd8a976273726
Pablo Troli <brawlixo123@gmail.com>
2025-03-19 01:07:23 -0300

    🛣️ feat (urls) Adds routes for zweet, like and comment details

--- zweets/urls.py
+++ zweets/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import FeedView, ZweetCreateView
+from .views import FeedView, ZweetCreateView, ZweetDetailView, LikeZweetView, AddCommentView
 
 app_name = "zweets"
 
 urlpatterns = [
     path("", FeedView.as_view(), name="feed"),
     path("create/", ZweetCreateView.as_view(), name="create"),
+    path("<int:pk>/", ZweetDetailView.as_view(), name="zweet_detail"),
+    path("<int:pk>/like/", LikeZweetView.as_view(), name="like_zweet"),
+    path("<int:pk>/comment/", AddCommentView.as_view(), name="add_comment"),
 ]
