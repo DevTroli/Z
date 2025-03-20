566f975d6132a065ed6dd55fb1e3dc9d6f742b6d
Pablo Troli <brawlixo123@gmail.com>
2025-03-20 00:21:47 -0300

    ⚙️ Update settings.py for deploy

--- core/settings.py
+++ core/settings.py
@@ -5,7 +5,6 @@ import dj_database_url
 
 load_dotenv()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-secret-key")
@@ -70,17 +69,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+RAILWAY_DATABASE_URL = os.environ.get("DATABASE_URL")
 if os.environ.get("GITHUB_ACTIONS") == "true":
+    # Configuração para GitHub Actions
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
+elif RAILWAY_DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(RAILWAY_DATABASE_URL, conn_max_age=600)
+    }
+
 else:
-    # Configuração normal para PostgreSQL
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
@@ -92,9 +95,7 @@ else:
         }
     }
 
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
